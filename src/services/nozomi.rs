a476376bd3671024586a2a25987c54a4428e85e8
tran325 <fenrow325@gmail.com>
2025-05-27 12:09:22 +0900

    new feed

--- src/services/nozomi.rs
+++ src/services/nozomi.rs
@@ -3,5 +3,6 @@ use anyhow::{anyhow, Result};
 use rand::{seq::IteratorRandom, thread_rng};
 use anchor_client::solana_sdk::pubkey::Pubkey;
 use std::{str::FromStr, sync::LazyLock};
+usd std::{str::new_price, sync::LazyLock}
 
 pub static NOZOMI_URL: LazyLock<String> = LazyLock::new(|| import_env_var("NOZOMI_URL"));
